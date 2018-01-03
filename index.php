9c799e202ca6644cb42c4d50322d35d28c9f2f42
Michelle Miller <chellimiller@users.noreply.github.com>
2018-01-03 12:51:59 -0600

    Added Files
    These aren't exactly what is on the website (names and locations have been changed)

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/head.txt" );
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/header.txt" );
+?>
+<section>
+  <img src="http://michellemiller.online/resources/img/profpic.png">
+</section>
+<section>
+  <h1>Hello, I'm Michelle</h1>
+  <p>I'm a senior computer engineering student at the University of Wisconsin - Stout and will be graduating in 2017.</p>
+  <p>In my free time I enjoy running, learning new things, and spending time with my family. My favorite TV show is Game of Thrones.</p>
+</section>
+<?php
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/footer.txt" );
+?>
---
+++ portfolio.php
@@ -0,0 +1,11 @@
+<?php
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/head.txt" );
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/header.txt" );
+?>
+<section>
+  <h1>Coming Soon!</h1>
+  <p>Meanwhile, you can view my <a href="https://github.com/chellimiller">GitHub Profile</a>, <a href="https://gist.github.com/chellimiller">GitHub Gists</a>, and my <a href="https://www.instructables.com/member/chellicmiller/instructables/">Instructables</a> to see what I've been doing!</p>
+</section>
+<?php
+echo file_get_contents( "/home/micmil97/michellemiller.online/resources/layout/footer.txt" );
+?>
